40f16d0606875fcb8ab250a1585b18071d8478f9
Kenji Yamauchi <yamakenji@gmail.com>
2022-02-13 22:33:36 +0900

    add: メモリと確保

--- ch4_ownership/src/main.rs
+++ ch4_ownership/src/main.rs
@@ -7,4 +7,20 @@ fn main() {
     let mut s_mut = String::from("hello");
     s_mut.push_str(",world!");
     println!("{}", s_mut);
+
+    // move
+
+    // List4-2
+    // スタックに積まれる
+    let x = 5;
+    let y = x;
+
+    println!("x is {}, y is {}", x, y);
+
+    let s1 = String::from("hello");
+    // move
+    // let s2 = s1;
+    // clone
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }

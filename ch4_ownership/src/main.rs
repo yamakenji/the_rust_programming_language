3d8d114989c995278de76c71798eabb79f8f7f19
Kenji Yamauchi <yamakenji@gmail.com>
2022-02-19 21:09:21 +0900

    mod: ch4_ownership

--- ch4_ownership/src/main.rs
+++ ch4_ownership/src/main.rs
@@ -1,26 +1,65 @@
 fn main() {
-    println!("Hello, world!");
+    // println!("Hello, world!");
 
+    // let s = String::from("hello");
+    // println!("{}", s);
+
+    // let mut s_mut = String::from("hello");
+    // s_mut.push_str(",world!");
+    // println!("{}", s_mut);
+
+    // // move
+
+    // // List4-2
+    // // スタックに積まれる
+    // let x = 5;
+    // let y = x;
+
+    // println!("x is {}, y is {}", x, y);
+
+    // let s1 = String::from("hello");
+    // // move
+    // // let s2 = s1;
+    // // clone
+    // let s2 = s1.clone();
+    // println!("s1 = {}, s2 = {}", s1, s2);
+
+    // 所有権と関数
     let s = String::from("hello");
-    println!("{}", s);
 
-    let mut s_mut = String::from("hello");
-    s_mut.push_str(",world!");
-    println!("{}", s_mut);
+    takes_ownership(s);
 
-    // move
+    // println!("{}", s); // ここではsは使えない！
 
-    // List4-2
-    // スタックに積まれる
     let x = 5;
-    let y = x;
+    makes_copy(x);
 
-    println!("x is {}, y is {}", x, y);
+    println!("{}", x); //ｘは大丈夫
 
-    let s1 = String::from("hello");
-    // move
-    // let s2 = s1;
-    // clone
-    let s2 = s1.clone();
-    println!("s1 = {}, s2 = {}", s1, s2);
+    // 戻り値とスコープ
+    let s1 = gives_ownership();
+
+    let s2 = String::from("hello");
+
+    let s3 = takes_and_gives_back(s2);
+    println!("{}", s1);
+    // println!("{}", s2); s2はs3にムーブされる
+    println!("{}", s3);
+}
+
+fn takes_ownership(some_string: String) {
+    println!("{}", some_string);
+}
+
+fn makes_copy(some_integer: i32) {
+    println!("{}", some_integer);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
 }

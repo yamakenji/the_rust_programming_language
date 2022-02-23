84df54f5ace971cdd51825e20c5cb58a14b14b2b
Kenji Yamauchi <yamakenji@gmail.com>
2022-02-23 21:26:44 +0900

    add: ch5_struct

---
+++ ch5_struct/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+    struct User {
+        username: String,
+        email: String,
+        sign_in_count: u64,
+        active: bool,
+    }
+
+    // インスタンスを生成する
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // 与えられたemailとusernameでインスタンスを生成する関数
+    fn build_user(email: String, username: String) -> User {
+        User {
+            email,
+            username,
+            active: true,
+            sign_in_count: 1,
+        }
+    }
+
+    // 更新記法で他のインスタンスからインスタンスを生成する
+
+    let user2 = User {
+        email: String::from("another@exmaple.com"),
+        username: String::from("anotherusername567"),
+        ..user1
+    };
+
+    
+
+}

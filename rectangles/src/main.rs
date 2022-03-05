22f939aa8a356b97d176c93c9a10b825a7a5fe6a
Kenji Yamauchi <yamakenji@gmail.com>
2022-03-05 15:07:52 +0900

    5.3 メソッド記法

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,6 +5,23 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
     // println!("Hello, world!");
 
@@ -19,14 +36,27 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {:#?}", rect1);
+    let sq = Rectangle::square(3);
+    // println!("rect1 is {:#?}", rect1);
+    println!("sq is {:#?}", sq);
 
     println!(
         "The area of the rectangle is {} square pixels.",
         // area(width1, height1)
-        area(&rect1)
+        rect1.area()
     );
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
 
 // fn area(width: u32, height: u32) -> u32 {
@@ -35,6 +65,6 @@ fn main() {
 // fn area(dimensions: (u32, u32)) -> u32 {
 //     dimensions.0 * dimensions.1
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }

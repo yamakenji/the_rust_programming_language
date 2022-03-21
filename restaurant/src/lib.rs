592d8b0dda135de2b0dc5d80e93b3186a2747cb3
Kenji Yamauchi <yamakenji@gmail.com>
2022-03-21 14:54:08 +0900

    相対パスをsuperで始める,構造体とenumを公開する

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -11,7 +11,36 @@ mod front_of_house {
         fn serve_order() {}
         fn take_payment() {}
 
-        mod back_of_house {}
+        // mod back_of_house {
+        //     fn fix_incorrect_order() {
+        //         cook_order();
+        //         super::serve_order();
+        //     }
+
+        //     fn cook_order() {}
+        // }
+    }
+}
+
+// 構造体とenumを公開する
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
     }
 }
 
@@ -21,6 +50,18 @@ pub fn eat_at_restaurant() {
 
     // 相対パス
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please.", meal.toast);
+
+    // We're not allowed to see or modify the seasonal fruit.
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 // #[cfg(test)]
